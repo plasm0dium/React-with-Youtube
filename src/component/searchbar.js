81227cc143fd8486fbeb2e08359aeaf24a387889
plasm0dium <teddyly1988@gmail.com>
2016-02-12 17:09:25 -0800

    [update] App to class, add youtube search ajax call

---
+++ src/component/searchbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    // Calling parent method with super
+    super(props);
+
+    this.state = { term: '' };
+  }
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    )
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import YouTubeSearch from "youtube-api-search";
+import SearchBar from "./component/searchbar";
 
 const API_KEY = 'AIzaSyCcXK8dEx2zLC1rb-cSKiaxlttEkIzysJk';
 
-const App = () => {
-  return <div>Hi!</div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YouTubeSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
